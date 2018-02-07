18e35f1d6927fd81007287e9fbfb45de65fc6050
alexuk86 <34319714+alexuk86@users.noreply.github.com>
2018-02-07 18:22:12 +0000

    Reduced extrution rate

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -536,7 +536,7 @@
  * Override with M92
  *                                      X, Y, Z, E0 [, E1[, E2[, E3[, E4]]]]
  */
-#define DEFAULT_AXIS_STEPS_PER_UNIT   { 100.5, 100.5, 1600, 1100 }
+#define DEFAULT_AXIS_STEPS_PER_UNIT   { 100.5, 100.5, 1600, 1000 }
 
 /**
  * Default Max Feed Rate (mm/s)

31055f1648d5715395d3d73288f016da6aa83209
alexuk86 <34319714+alexuk86@users.noreply.github.com>
2018-08-25 22:02:35 +0100

    Corrected overextrusion
    Previous miscalculation caused the number to increase, not decrease.

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -536,7 +536,7 @@
  * Override with M92
  *                                      X, Y, Z, E0 [, E1[, E2[, E3[, E4]]]]
  */
-#define DEFAULT_AXIS_STEPS_PER_UNIT   { 100.5, 100.5, 1600, 1040 }
+#define DEFAULT_AXIS_STEPS_PER_UNIT   { 100.5, 100.5, 1600, 866 }
 
 /**
  * Default Max Feed Rate (mm/s)

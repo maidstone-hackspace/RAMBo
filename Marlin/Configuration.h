1d783673b097c1e954ef9baf81ccd08b35ad0c44
alexuk86 <34319714+alexuk86@users.noreply.github.com>
2019-04-08 20:40:45 +0100

    Z probe offset after hotend cleanout

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -692,7 +692,7 @@
  */
 #define X_PROBE_OFFSET_FROM_EXTRUDER -54  // X offset: -left  +right  [of the nozzle]
 #define Y_PROBE_OFFSET_FROM_EXTRUDER 11  // Y offset: -front +behind [the nozzle]
-#define Z_PROBE_OFFSET_FROM_EXTRUDER -1.0   // Z offset: -below +above  [the nozzle]
+#define Z_PROBE_OFFSET_FROM_EXTRUDER -2.1   // Z offset: -below +above  [the nozzle]
 
 // X and Y axis travel speed (mm/m) between probes
 #define XY_PROBE_SPEED 8000
